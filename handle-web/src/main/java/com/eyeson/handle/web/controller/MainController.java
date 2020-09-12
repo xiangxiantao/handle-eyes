8c86d3d801c9d185ea4fdff49eff0f205d0794c8
haytt.xiang <haytt.xiang@xgimi.com>
2020-09-12 18:53:25 +0800

    slf4j MDC

--- handle-web/src/main/java/com/eyeson/handle/web/controller/MainController.java
+++ handle-web/src/main/java/com/eyeson/handle/web/controller/MainController.java
@@ -2,6 +2,10 @@ package com.eyeson.handle.web.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.slf4j.impl.StaticMDCBinder;
+import org.slf4j.spi.MDCAdapter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +22,14 @@ import java.util.Date;
 */
 @Api(value = "基础服务")
 @RestController
+@Slf4j
 public class MainController {
 
     @ApiOperation(value = "欢迎信息")
     @GetMapping("/welcome")
     public String welcome(@RequestParam("user") String user){
+        MDC.put("user", user);
+        log.info("这是普通日志内容");
         return "welcome";
     }
 
